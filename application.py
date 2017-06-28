5e4148523371a9b2054b00174b865cb5bac34868
ahn94 <ahn94@hotmail.com>
2017-06-28 15:45:26 -0500

    added about page. Added average line to wpm chart.

--- application.py
+++ application.py
@@ -6,8 +6,13 @@ JSGlue(app)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
--- static/scripts.js
+++ static/scripts.js
@@ -1,9 +1,9 @@
 var chartWPM;
 var chartAcc;
 
-$(function () { 
+$(function () {
     console.log("working");
-    
+
     // Check for the various File API support.
     if (window.File && window.FileReader && window.FileList && window.Blob) {
         // Great success! All the File APIs are supported.
@@ -11,8 +11,18 @@ $(function () {
     } else {
         alert('The File APIs are not fully supported in this browser.');
     }
-    
-    $('#submit').on("click",function(e){
+
+    $('#sample').on('click', function () {
+        $.ajax({
+            url: "/static/race_data.csv",
+            async: false,
+            success: function (data) {
+                parseCSV(data);
+            }
+        });
+    });
+
+    $('#submit').on("click", function (e) {
         e.preventDefault();
         var file = $('#files').get(0).files[0];
         parseCSV(file);
@@ -22,39 +32,59 @@ $(function () {
 
 
 function parseCSV(file) {
-    
+
     if (!file) {
         alert('Please select a file!');
         return;
     }
-    
+
     Papa.parse(file, {
         config: {
             delimiter: "auto",
             complete: toChart
         },
-        error: function(err, file)
-        {
+        error: function (err, file) {
             console.log("ERROR:", err, file);
             throw(err);
         },
-        complete: function(results, file) {
+        complete: function (results, file) {
             console.log("Parsing complete:", results, file);
             toChart(results, file);
-            
+
         }
     });
 }
 
+function avgArr(array, start, end) {
+    var sum = 0;
+    var i = start;
+    for (; i < end; i++) {
+        sum = sum + array[i][0] / 1;
+    }
+    return sum / (end - start);
+}
+
 function toChart(json, file) {
-    
     var wpm = [];
+    var avg = [];
     var acc = [];
-    
+
+    var chunk = parseInt(json.data.length / 40, 10);
+
     for (var i = 1; i < json.data.length; i++) {
         wpm.push([parseInt(json.data[i][1], 10)]);
         acc.push([parseFloat(json.data[i][2]), parseInt(json.data[i][1], 10)]);
     }
+    for (var n = 0; n < wpm.length; n++) {
+        if ( n % chunk === 0) {
+            if (n + chunk > wpm.length) {
+                avg.push([n, avgArr(wpm, n, wpm.length - 1)]);
+            } else {
+                avg.push([n, avgArr(wpm, n, n + chunk)]);
+            }
+        }
+    }
+
 
     $('#container').removeClass('gone');
     chartWPM = Highcharts.chart('container', {
@@ -76,7 +106,7 @@ function toChart(json, file) {
             },
             minTickInterval: 1
         },
-        tooltip: { 
+        tooltip: {
             enabled: true,
             pointFormat: "{series.name}: <b>{point.y}</b><br/>",
             animation: false,
@@ -95,9 +125,17 @@ function toChart(json, file) {
             marker: {
                 radius: 6
             }
+        }, {
+            name: 'AVG',
+            color: 'rgba(15, 30, 80, .5)',
+            marker: {
+                radius: 0
+            },
+            type: 'spline',
+            data: avg
         }]
     });
-    
+
     $('#container2').removeClass('gone');
     chartWPM = Highcharts.chart('container2', {
         chart: {
@@ -117,7 +155,7 @@ function toChart(json, file) {
             },
             minTickInterval: .005
         },
-        tooltip: { 
+        tooltip: {
             enabled: true,
             pointFormat: "{series.name}: <b>{point.y}</b><br/>",
             animation: false,
@@ -138,6 +176,6 @@ function toChart(json, file) {
             }
         }]
     });
-    
-    
+
+
 }
